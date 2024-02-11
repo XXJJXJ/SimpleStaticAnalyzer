7501ccbaeca436c1c425c79839683aff26e88589
dt-td <e0543463@u.nus.edu>
2024-02-11 19:32:15 +0800

    Add sp.cpp and sp.h

---
+++ Team03/Code03/src/spa/src/sp/sp.cpp
@@ -0,0 +1,32 @@
+#include "Sp.h"
+#include <common/Program.h>
+#include <sp/parser/Parser.h>
+#include <common/spa_exception/SpaException.h>
+#include "sp/tokenizer/Tokenizer.h"
+
+#include <iostream>
+#include <filesystem>
+
+Sp::Sp() {}
+
+bool Sp::ProcessSIMPLE(string fileName) {
+    try {
+        ifstream inputfile(fileName);
+        if (!inputfile.is_open()) {
+            std::cerr << "Error opening file" << std::endl;
+        }
+        else {
+            shared_ptr<Tokenizer> tokenizer = make_shared<Tokenizer>();
+            vector<shared_ptr<Token>> tokens = tokenizer->tokenize(inputfile);
+            shared_ptr<Parser> parser = make_shared<Parser>();
+            shared_ptr<Program> program = parser->parseSource(tokens);
+            Program::ProcedureListContainer procedures = program->getProcedureList();
+            vector<shared_ptr<Statement>> statements = procedures[0]->getStatementList();
+            return true;
+        }
+    }
+    catch (SpaException& e) {
+        std::cout << e.what() << std::endl;
+        return false;
+    }
+}
