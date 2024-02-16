bdaae6a15721ca8fca72d15cbc6810e6a616d2c3
Stephanie Chen <stephaniechenshiyao@gmail.com>
2024-02-16 22:03:58 +0800

    Fixed error thrown when running autotester

--- Team03/Code03/src/spa/src/qps/entity/parser/DeclarationsParser.cpp
+++ Team03/Code03/src/spa/src/qps/entity/parser/DeclarationsParser.cpp
@@ -5,6 +5,10 @@ DeclarationsParser::DeclarationsParser() {}
 DeclarationsParser::~DeclarationsParser() {}
 std::vector<std::shared_ptr<Synonym>> DeclarationsParser::parse(std::vector<std::string> tokens) {
 	//Todo: implement
-	std::vector<std::shared_ptr<Synonym>> declarations;
+	std::vector<std::shared_ptr<Synonym>> declarations = {};
+	Synonym syn(EntityType::Variable, "variable");
+	std::shared_ptr<Synonym> sharedSyn = std::make_shared<Synonym>(syn);
+	declarations.push_back(sharedSyn);
+
 	return declarations;
 }
--- Team03/Code03/src/spa/src/qps/entity/parser/SelectionsParser.cpp
+++ Team03/Code03/src/spa/src/qps/entity/parser/SelectionsParser.cpp
@@ -5,6 +5,10 @@ SelectionsParser::SelectionsParser() {}
 SelectionsParser::~SelectionsParser() {}
 std::vector<std::shared_ptr<Synonym>> SelectionsParser::parse(std::vector<std::string> tokens) {
 	//Todo: implement
-	std::vector<std::shared_ptr<Synonym>> selections;
+	std::vector<std::shared_ptr<Synonym>> selections = {};
+	Synonym syn(EntityType::Variable, "variable");
+	std::shared_ptr<Synonym> sharedSyn = std::make_shared<Synonym>(syn);
+	selections.push_back(sharedSyn);
+	
 	return selections;
 }
