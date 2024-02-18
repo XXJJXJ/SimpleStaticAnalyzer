30868c80f37fcd3c673a3d6091fca580befb03bd
Stephanie Chen <stephaniechenshiyao@gmail.com>
2024-02-19 01:03:59 +0800

    Implemented SelectionsParser

--- Team03/Code03/src/spa/src/qps/entity/parser/SelectionsParser.cpp
+++ Team03/Code03/src/spa/src/qps/entity/parser/SelectionsParser.cpp
@@ -1,14 +1,48 @@
 #include "SelectionsParser.h"
 #include "common/EntityType.h"
+#include "common/spa_exception/SemanticErrorException.h"
+#include "common/spa_exception/SyntaxErrorException.h"
+#include <memory>
 
 SelectionsParser::SelectionsParser() {}
 SelectionsParser::~SelectionsParser() {}
 std::vector<std::shared_ptr<Synonym>> SelectionsParser::parse(const std::vector<std::string>& tokens, const std::unordered_map<std::string, EntityType>& synonymMap) {
 	//Todo: implement
 	std::vector<std::shared_ptr<Synonym>> selections = {};
-	Synonym syn(EntityType::Variable, "variable");
-	std::shared_ptr<Synonym> sharedSyn = std::make_shared<Synonym>(syn);
-	selections.push_back(sharedSyn);
+	size_t len = tokens.size();
+
+	if (tokens[1] == "<" && tokens[len-1] == ">" && len > 3) {
+		bool isSyn = true;
+		for (size_t i = 2; i < len - 1; i++) {
+			std::string token = tokens[i];
+			if (isSyn) {
+				EntityType synonymType = synonymMap.at(token);
+				std::shared_ptr<Synonym> synonym = std::make_shared<Synonym>(synonymType, token);
+				selections.push_back(synonym);
+				isSyn = false;
+			}
+			else if (token == "," && i < len-2) {
+				isSyn = true;
+			}
+			else {
+				throw SyntaxErrorException("Invalid selection syntax");
+			}
+		}
+	} 
+	else if (len == 2) {
+		std::string synonymName = tokens[1];
+		try {
+			EntityType synonymType = synonymMap.at(synonymName);
+			std::shared_ptr<Synonym> synonym = std::make_shared<Synonym>(synonymType, synonymName);
+			selections.push_back(synonym);
+		}
+		catch (const std::out_of_range& e) {
+			throw SemanticErrorException("Selected synonym '" + synonymName + "' has not been declared");
+		}
+	}
+	else {
+		throw SyntaxErrorException("Invalid selection syntax");
+	}
 	
 	return selections;
 }
