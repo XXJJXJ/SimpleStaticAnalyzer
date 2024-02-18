5b8299b68689c17ddb7ed3dd3c4ad89288285eef
Stephanie Chen <stephaniechenshiyao@gmail.com>
2024-02-19 01:46:25 +0800

    Add comments for SelectionsParser for clarity

--- Team03/Code03/src/spa/src/qps/entity/parser/SelectionsParser.cpp
+++ Team03/Code03/src/spa/src/qps/entity/parser/SelectionsParser.cpp
@@ -10,8 +10,10 @@ std::vector<std::shared_ptr<Synonym>> SelectionsParser::parse(const std::vector<
 	std::vector<std::shared_ptr<Synonym>> selections = {};
 	size_t len = tokens.size();
 
+	// Select tuple
 	if (tokens[1] == "<" && tokens[len-1] == ">" && len > 3) {
 		bool isSyn = true;
+		// tokens within "<" and ">" must start and end with synonym, alternate between synonym and ","
 		for (size_t i = 2; i < len - 1; i++) {
 			std::string token = tokens[i];
 			if (isSyn) {
@@ -33,6 +35,7 @@ std::vector<std::shared_ptr<Synonym>> SelectionsParser::parse(const std::vector<
 			}
 		}
 	} 
+	// Select a single synonym
 	else if (len == 2) {
 		std::string synonymName = tokens[1];
 		try {
