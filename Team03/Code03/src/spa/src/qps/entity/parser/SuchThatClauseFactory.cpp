5da0c1bad672c2822c592bc59040e85423cec21a
Stephanie Chen <stephaniechenshiyao@gmail.com>
2024-02-21 14:48:11 +0800

    Remove `such that` before passing to predicate factory

--- Team03/Code03/src/spa/src/qps/entity/parser/SuchThatClauseFactory.cpp
+++ Team03/Code03/src/spa/src/qps/entity/parser/SuchThatClauseFactory.cpp
@@ -1,5 +1,6 @@
 #include "SuchThatClauseFactory.h"
 #include "PredicateFactoryManager.h"
+#include "common/spa_exception/SyntaxErrorException.h"
 
 SuchThatClauseFactory::SuchThatClauseFactory() {}
 SuchThatClauseFactory::~SuchThatClauseFactory() {}
@@ -7,14 +8,22 @@ SuchThatClauseFactory::~SuchThatClauseFactory() {}
 std::shared_ptr<Clause> SuchThatClauseFactory::createClauseObject(const std::vector<std::string>& tokens) {
 	SuchThatClause suchThatClause;
 
-	std::string firstWord = tokens[2];
-	PredicateFactory* factory = PredicateFactoryManager::getPredicateFactory(firstWord);
-	if (factory) {
-		std::unique_ptr<Predicate> predicateObject = factory->createPredicate(tokens);
-		suchThatClause.addPredicate(std::move(predicateObject));
+	if (tokens[1] == "that" && tokens.size() > 2) {
+		std::string firstWord = tokens[2];
+		std::vector<std::string> predicateTokens(tokens.begin() + 2, tokens.end());
+
+		PredicateFactory* factory = PredicateFactoryManager::getPredicateFactory(firstWord);
+		if (factory) {
+			std::unique_ptr<Predicate> predicateObject = factory->createPredicate(tokens);
+			suchThatClause.addPredicate(std::move(predicateObject));
+		}
+
+		std::shared_ptr<SuchThatClause> sharedSuchThat = std::make_shared<SuchThatClause>(suchThatClause);
+
+		return sharedSuchThat;
 	}
-
-	std::shared_ptr<SuchThatClause> sharedSuchThat = std::make_shared<SuchThatClause>(suchThatClause);
-
-	return sharedSuchThat;
+	else {
+		throw SyntaxErrorException("Invalid such that clause syntax");
+	}
+	
 }
