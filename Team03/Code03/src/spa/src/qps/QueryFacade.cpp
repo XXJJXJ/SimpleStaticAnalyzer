44933efd4b90ff2b56bdb899ba24a757339315cf
zt <58112399+zhengtaoJ@users.noreply.github.com>
2024-02-24 13:02:39 +0800

    Add planner logic in facade

--- Team03/Code03/src/spa/src/qps/QueryFacade.cpp
+++ Team03/Code03/src/spa/src/qps/QueryFacade.cpp
@@ -1,12 +1,10 @@
-/*
-* Created by ZHENGTAO JIANG on 8/2/24.
-*/
 
 #include "QueryFacade.h"
 #include "QueryEvaluator.h"
 #include "QueryParser.h"
 #include "QueryValidator.h"
 #include "QueryTokenizer.h"
+#include "qps/entity/evaluation/EvaluationPlanner.h"
 #include "common/spa_exception/SyntaxErrorException.h"
 #include "common/spa_exception/SemanticErrorException.h"
 #include <iostream>
@@ -40,6 +38,8 @@ std::shared_ptr<Query> QueryFacade::parseQuery(std::vector<std::vector<std::vect
 }
 
 vector<std::string> QueryFacade::evaluateQuery(const std::shared_ptr<Query> parsedQuery) {
-    QueryEvaluator evaluator = QueryEvaluator();
-    return evaluator.evaluate(parsedQuery);
+    auto evaluator = make_shared<QueryEvaluator>();
+    auto planner = EvaluationPlanner(parsedQuery, evaluator);
+    planner.plan();
+    return evaluator->evaluate(parsedQuery);
 }
