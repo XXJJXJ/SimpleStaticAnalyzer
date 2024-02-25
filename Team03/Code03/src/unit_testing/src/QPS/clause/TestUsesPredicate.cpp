135efac34a42f9202ed6d4ed70791e137c8971f1
Stephanie Chen <stephaniechenshiyao@gmail.com>
2024-02-25 22:26:48 +0800

    add TestUsesPredicate unit tests

--- Team03/Code03/src/unit_testing/src/QPS/clause/TestUsesPredicate.cpp
+++ Team03/Code03/src/unit_testing/src/QPS/clause/TestUsesPredicate.cpp
@@ -3,7 +3,7 @@
 #include "qps/entity/query/Synonym.h"
 #include "qps/entity/evaluation/BooleanTable.h"
 #include "qps/entity/clause/UsesPredicate.h"
-
+#include "qps/entity/evaluation/HeaderTable.h"
 #include "../fakeEntities/FakeQueryManager.cpp"
 
 
@@ -51,7 +51,43 @@ TEST_CASE("Test table retrieval 2", "[UsesPredicate]") {
         }
     }
 
-    SECTION("Using synonyms only") {
-        
+    SECTION("Using synonyms only - Uses(s1, v) get 2 rows") {
+        Synonym stmtSyn(EntityType::Stmt, "s1");
+        Synonym varSyn(EntityType::Variable, "v");
+        UsesPredicate usesPred(stmtSyn, varSyn);
+        auto table = usesPred.getTable(qm);
+        auto headerTable = dynamic_pointer_cast<HeaderTable>(table);
+        REQUIRE(headerTable->getRows().size() == 2);
+    }
+    SECTION("Using pure wildcards") {
+        SECTION("Uses(_, _) - get true") {
+            UsesPredicate usesPred("_", "_");
+            auto table = usesPred.getTable(qm);
+            auto boolTable = dynamic_pointer_cast<BooleanTable>(table);
+            REQUIRE(boolTable->isTrue());
+        }
+        SECTION("Uses(_, _) - with empty table get false") {
+            FakeQueryManager qm2; // empty qm
+            UsesPredicate usesPred("_", "_");
+            auto table = usesPred.getTable(qm2);
+            auto boolTable = dynamic_pointer_cast<BooleanTable>(table);
+            REQUIRE(!boolTable->isTrue());
+        }
+    }
+    SECTION("Using partial wildcards") {
+        SECTION("Uses(s1, _) - get 2 rows") {
+            Synonym stmtSyn(EntityType::Stmt, "s1");
+            UsesPredicate usesPred(stmtSyn, "_");
+            auto table = usesPred.getTable(qm);
+            auto headerTable = dynamic_pointer_cast<HeaderTable>(table);
+            REQUIRE(headerTable->getRows().size() == 2);
+        }
+        SECTION("Uses(_, v) - get 2 rows") {
+            Synonym varSyn(EntityType::Variable, "v1");
+            UsesPredicate usesPred("_", varSyn);
+            auto table = usesPred.getTable(qm);
+            auto headerTable = dynamic_pointer_cast<HeaderTable>(table);
+            REQUIRE(headerTable->getRows().size() == 2);
+        }
     }
 }
