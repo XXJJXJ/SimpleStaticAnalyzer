e6480d40f8f71c9910143013bd130522ba896008
Ye Xinjian <e0544100@u.nus.edu>
2024-03-02 11:44:21 +0800

    Add Call Statement Store

--- Team03/Code03/src/spa/src/pkb/entity_store/StatementStore.cpp
+++ Team03/Code03/src/spa/src/pkb/entity_store/StatementStore.cpp
@@ -2,13 +2,66 @@
 
 StatementStore::StatementStore() : lineToStatementMap() {}
 
+bool StatementStore::checkStmt(shared_ptr<Statement> stmt) {
+    return lineToStatementMap.find(stmt->getStatementNumber()) != lineToStatementMap.end();
+}
+
 bool StatementStore::add(shared_ptr<Statement> stmt) {
-    if (lineToStatementMap.find(stmt->getStatementNumber()) != lineToStatementMap.end()) {
+    if (checkStmt(stmt)) {
         return false;
     }
     lineToStatementMap[stmt->getStatementNumber()] = stmt;
     return true;
 }
+
+bool StatementStore::addAssign(shared_ptr<AssignStatement> stmt) {
+    if (add(stmt)) {
+        lineToAssignStatementMap[stmt->getStatementNumber()] = stmt;
+        return true;
+    }
+    return false;
+}
+
+bool StatementStore::addCall(shared_ptr<CallStatement> stmt) {
+    if (add(stmt)) {
+        lineToCallStatementMap[stmt->getStatementNumber()] = stmt;
+        return true;
+    }
+    return false;
+}
+
+bool StatementStore::addIf(shared_ptr<IfStatement> stmt) {
+    if (add(stmt)) {
+        lineToIfStatementMap[stmt->getStatementNumber()] = stmt;
+        return true;
+    }
+    return false;
+}
+
+bool StatementStore::addRead(shared_ptr<ReadStatement> stmt) {
+    if (add(stmt)) {
+        lineToReadStatementMap[stmt->getStatementNumber()] = stmt;
+        return true;
+    }
+    return false;
+}
+
+bool StatementStore::addPrint(shared_ptr<PrintStatement> stmt) {
+    if (add(stmt)) {
+        lineToPrintStatementMap[stmt->getStatementNumber()] = stmt;
+        return true;
+    }
+    return false;
+}
+
+bool StatementStore::addWhile(shared_ptr<WhileStatement> stmt) {
+    if (add(stmt)) {
+        lineToWhileStatementMap[stmt->getStatementNumber()] = stmt;
+        return true;
+    }
+    return false;
+}
+
 vector<shared_ptr<Statement>> StatementStore::getAll() const {
     vector<shared_ptr<Statement>> allStatements;
     for (const auto& item : lineToStatementMap) {
@@ -16,8 +69,63 @@ vector<shared_ptr<Statement>> StatementStore::getAll() const {
     }
     return allStatements;
 }
+
+vector<shared_ptr<AssignStatement>> StatementStore::getAllAssign() const {
+    vector<shared_ptr<AssignStatement>> allStatements;
+    for (const auto& item : lineToAssignStatementMap) {
+        allStatements.push_back(item.second);
+    }
+    return allStatements;
+}
+
+vector<shared_ptr<CallStatement>> StatementStore::getAllCall() const {
+    vector<shared_ptr<CallStatement>> allStatements;
+    for (const auto& item : lineToCallStatementMap) {
+        allStatements.push_back(item.second);
+    }
+    return allStatements;
+}
+
+vector<shared_ptr<IfStatement>> StatementStore::getAllIf() const {
+    vector<shared_ptr<IfStatement>> allStatements;
+    for (const auto& item : lineToIfStatementMap) {
+        allStatements.push_back(item.second);
+    }
+    return allStatements;
+}
+
+vector<shared_ptr<PrintStatement>> StatementStore::getAllPrint() const {
+    vector<shared_ptr<PrintStatement>> allStatements;
+    for (const auto& item : lineToPrintStatementMap) {
+        allStatements.push_back(item.second);
+    }
+    return allStatements;
+}
+
+vector<shared_ptr<ReadStatement>> StatementStore::getAllRead() const {
+    vector<shared_ptr<ReadStatement>> allStatements;
+    for (const auto& item : lineToReadStatementMap) {
+        allStatements.push_back(item.second);
+    }
+    return allStatements;
+}
+
+vector<shared_ptr<WhileStatement>> StatementStore::getAllWhile() const {
+    vector<shared_ptr<WhileStatement>> allStatements;
+    for (const auto& item : lineToWhileStatementMap) {
+        allStatements.push_back(item.second);
+    }
+    return allStatements;
+}
+
 void StatementStore::clear() {
     lineToStatementMap.clear();
+    lineToAssignStatementMap.clear();
+    lineToCallStatementMap.clear();
+    lineToPrintStatementMap.clear();
+    lineToIfStatementMap.clear();
+    lineToReadStatementMap.clear();
+    lineToWhileStatementMap.clear();
 }
 
 StatementStore::~StatementStore() {
