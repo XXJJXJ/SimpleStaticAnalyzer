8c929127d7bdf7e415116920f769955dda64b04e
zt <58112399+zhengtaoJ@users.noreply.github.com>
2024-03-09 18:15:33 +0800

    delete some files

--- Team03/Code03/src/spa/src/qps/entity/clause/cellFilter/WildcardFilter-ZHENGTAO’s MacBook Pro.cpp
+++
@@ -1,5 +0,0 @@
-#include "WildcardFilter.h"
-
-bool WildcardFilter::passFilter(const std::shared_ptr<Entity>& entity) const {
-    return entity != nullptr;
-}
--- Team03/Code03/src/spa/src/qps/entity/clause/cellFilter/WildcardFilter-ZHENGTAO’s MacBook Pro.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "CellFilter.h"
-#include <memory>
-
-class WildcardFilter : public CellFilter {
-public:
-    WildcardFilter() = default;
-    bool passFilter(const std::shared_ptr<Entity>& entity) const override;
-};
