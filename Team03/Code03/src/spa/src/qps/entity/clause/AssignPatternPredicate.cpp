8519819c5deaab28dd3c2fcd87f45652259f574d
zt <58112399+zhengtaoJ@users.noreply.github.com>
2024-03-09 18:10:38 +0800

    Integrate the changes in assign pattern

--- Team03/Code03/src/spa/src/qps/entity/clause/AssignPatternPredicate.cpp
+++ Team03/Code03/src/spa/src/qps/entity/clause/AssignPatternPredicate.cpp
@@ -33,30 +33,9 @@ bool AssignPatternPredicate::isValidRhs(const std::string& rhs) {
 }
 
 std::shared_ptr<BaseTable> AssignPatternPredicate::getFullTable(QueryManager &qm) {
-    // TODO: move this logic to PKB, and we need a interface that takes in (expr, hasWildcard), and return a
-    //  vector<vector<shared_ptr<Entity>>>, where each row contains (assignStmt, variable).
-    // Step 1: get all possible LHS (arg1)
-    vector<shared_ptr<Entity>> allPossibleLhs;
-    if (holds_alternative<string>(lhs) && get<string>(lhs) != WILDCARD) {
-        // We don't have access to the internal of PKB here, so we can only create a new variable with the same name
-        allPossibleLhs.push_back(make_shared<Variable>(get<string>(lhs)));
-    } else {
-        allPossibleLhs = qm.getAllEntitiesByType(EntityType::Variable);
-    }
 
-    // Step 2: construct the table
-    BaseTable table;
-    table.setColumnCount(2);
-    for (auto& arg : allPossibleLhs) {
-        auto assignments = qm.getAssignPattern(stripWildcard(rhs), hasWildcard(rhs));
-        for (auto& assignment : assignments) {
-            // Only need to filter arg
-            if (arg->isOfType(EntityType::Variable)) {
-                table.addRow(TableRow({assignment[0], arg}));
-            }
-        }
-    }
-    return make_shared<BaseTable>(table);
+    return make_shared<BaseTable>(
+            qm.getAssignPattern(stripWildcard(rhs), hasWildcard(rhs)), 2);
 }
 
 
