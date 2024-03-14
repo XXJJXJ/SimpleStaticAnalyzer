d0f8d524b3d18883b06e8994dde2b3e883ea2308
Ye Xinjian <e0544100@u.nus.edu>
2024-03-14 18:15:55 +0800

    Update StmtStmtStore

--- Team03/Code03/src/spa/src/pkb/abstraction_store/StmtStmtStore.h
+++ Team03/Code03/src/spa/src/pkb/abstraction_store/StmtStmtStore.h
@@ -11,12 +11,15 @@ class StmtStmtStore {
 protected:
     unordered_map<shared_ptr<Statement>, unordered_set<shared_ptr<Statement>>> directMap;
     unordered_map<shared_ptr<Statement>, unordered_set<shared_ptr<Statement>>> transitiveMap;
+
+    // helper function
+    vector<vector<shared_ptr<Entity>>> getStmtPairs(const unordered_map<shared_ptr<Statement>, unordered_set<shared_ptr<Statement>>>& table);
 public:
     virtual bool add(shared_ptr<Statement> stmt1, shared_ptr<Statement> stmt2) = 0;
     virtual void clear() = 0;
     // using stmt - vector to accomodate parent
     vector<vector<shared_ptr<Entity>>> getDirect();
-    vector<vector<shared_ptr<Entity>>> getTransitive();
+    virtual vector<vector<shared_ptr<Entity>>> getTransitive();
     unordered_map<shared_ptr<Statement>, unordered_set<shared_ptr<Statement>>> getDirectMap();
     unordered_map<shared_ptr<Statement>, unordered_set<shared_ptr<Statement>>> getTransitiveMap();
 };
