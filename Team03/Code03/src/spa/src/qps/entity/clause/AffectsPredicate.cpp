de36e6b1d0476f1ff4b18e53b55499b1f1b7f59c
Ye Xinjian <e0544100@u.nus.edu>
2024-03-27 20:25:12 +0800

    Add AffectsPredicate

---
+++ Team03/Code03/src/spa/src/qps/entity/clause/AffectsPredicate.cpp
@@ -0,0 +1,16 @@
+#include "AffectsPredicate.h"
+
+std::shared_ptr<BaseTable> AffectsPredicate::getFullTable(QueryManager &qm) {
+    return std::make_shared<BaseTable>(qm.getAffects(), 2);
+}
+
+AffectsPredicate::AffectsPredicate(StatementRef lhs, StatementRef rhs) {
+    if (!isValidStatementRef(lhs) || !isValidStatementRef(rhs)) {
+        throw SemanticErrorException("Invalid argument for AffectsPredicate constructor");
+    }
+
+    this->lhs = std::move(lhs);
+    this->rhs = std::move(rhs);
+    addStmtRef(this->lhs);
+    addStmtRef(this->rhs);
+}
---
+++ Team03/Code03/src/spa/src/qps/entity/clause/AffectsPredicate.h
@@ -0,0 +1,18 @@
+#ifndef SPA_AFFECTSPREDICATE_H
+#define SPA_AFFECTSPREDICATE_H
+#include "Predicate.h"
+
+
+class AffectsPredicate : public Predicate {
+private:
+    StatementRef lhs;
+    StatementRef rhs;
+protected:
+    std::shared_ptr<BaseTable> getFullTable(QueryManager& qm) override;
+
+public:
+    AffectsPredicate(StatementRef lhs, StatementRef rhs);
+};
+
+
+#endif //SPA_AFFECTSPREDICATE_H
