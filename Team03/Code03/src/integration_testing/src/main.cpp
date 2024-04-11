ad4eed86d73cc78461085fde2a83cf61aaa68b88
mukundrs <mukundrs@hotmail.com>
2024-04-11 13:51:08 +0800

    FIx integration_testing build error + autotester test cases for with clause

--- Team03/Code03/src/integration_testing/src/main.cpp
+++ Team03/Code03/src/integration_testing/src/main.cpp
@@ -1,7 +1,5 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "sp/sp.h"
-#include "qps/QpsManager.h"
 
 
 // TODO: Remove this before final submission
